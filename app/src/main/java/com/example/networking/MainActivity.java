933dc29402ee75a09f3980033823482e63005333
there <a20thelo@student.his.se>
2021-04-28 15:30:54 +0200

    Initierade webview

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -12,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webbSettings=webView.getSettings();
+        webbSettings.setJavaScriptEnabled(true);
     }
 }
