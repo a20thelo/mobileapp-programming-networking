81e735c2db5328666877e5adc43b51ba697f8ec0
there <a20thelo@student.his.se>
2021-04-28 15:53:50 +0200

    Added gson libary

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,21 +3,40 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+
+    @SuppressWarnings("SameParameterValue")
+    private String readFile(String fileName) {
+        try {
+            //noinspection CharsetObjectCanBeUsed
+            return new Scanner(getApplicationContext().getAssets().open(fileName), Charset.forName("UTF-8").name()).useDelimiter("\\A").next();
+        } catch (IOException e) {
+            Log.e("DATA", "Could not read file: " + fileName);
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.webView);
+        webView = findViewById(R.id.webView1);
         webView.setWebViewClient(new WebViewClient());
         WebSettings webbSettings=webView.getSettings();
         webbSettings.setJavaScriptEnabled(true);
+
+        String s = readFile("mountains.json");
+        Log.d("DATA","The following text was found in textfile:\n\n"+s);
     }
 }
