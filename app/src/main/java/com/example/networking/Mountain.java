af7ce9d53d4b69c54a7b6687bb4c1f65b2a037f7
there <a20thelo@student.his.se>
2021-04-28 15:57:31 +0200

    Added java class, Mountain and Auxdata

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,16 @@
+package com.example.networking;
+
+public class Mountain {
+
+    private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+
+    private int size;
+    private int cost;
+
+    private Auxdata auxdata;
+}
